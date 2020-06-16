f08d9e2837c772c74567f3e884eccb615beefd2c
williamboco <yamyamboco21@gmail.com>
2020-06-16 15:43:03 +0800

    added an id

--- journal.py
+++ journal.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, redirect, request
-import datetime
+from datetime import datetime
 import json
 
 # initializing the application object
@@ -32,19 +32,18 @@ def login():
 @app.route('/add', methods=['GET', 'POST'])
 def add(filename='data.json'):
     if request.method == "POST":
-        # Serializing the Date Object via custom method
-        def default(obj):
-            if isinstance(obj, (datetime.date, datetime.datetime)):
-                return obj.isoformat()
 
         # Appending the form values
         with open("data.json") as json_file:
+            cont = json.load(json_file)
+
+            val = cont["journal"]
+            add_id = len(val) + 1
             title = request.form['title']
-            date = datetime.datetime.now().strftime('%Y-%m-%d')
+            date = datetime.now().strftime('%Y-%m-%d')
             body = request.form['body']
-            content = json.load(json_file)
-            val = content["journal"]
             values = {
+                "id": add_id,
                 "body": body,
                 "date": date,
                 "title": title
@@ -53,14 +52,14 @@ def add(filename='data.json'):
 
             # Writing the contents into the file
             with open(filename, 'w') as f:
-                json.dump(content, f, indent=4, sort_keys=True, default=default)
-                return redirect(url_for('home'))
-    return render_template('add.html', data=data)
+                json.dump(cont, f, indent=4, sort_keys=True)
+                home = '/home'
+
+            return redirect(home)
+    return render_template('add.html')
 
 
 # Delete Function
 @app.route('/delete')
 def delete():
-
-    data.remove()
-    return render_template('home.html', data=data)
+    return redirect(url_for('home'))
